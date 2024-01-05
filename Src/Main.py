bd93f35b1a89a9b492c3d4d81e0ee9452954049c
Elodie0207 <elodieguldas@gmail.com>
2024-01-05 10:37:57 +0100

    AjoutVelos

--- Src/Main.py
+++ Src/Main.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 
 repertoire_voitures = "..\\Voitures"
 repertoire_motos = "..\\Motos"
+repertoire_velos= "..\\Velos"
 output_repertoire = "..\\Test"
 
+np.random.seed(42)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
 class LinearModel:
     def __init__(self, learning_rate=0.00001, n_iterations=1000):
         self.learning_rate = learning_rate
@@ -14,9 +18,10 @@ class LinearModel:
         self.weights = None
         self.bias = None
 
-    def preprocess_images(self,repertoire_voitures, repertoire_motos, output_repertoire):
+    def preprocess_images(self, repertoire_voitures, repertoire_motos, repertoire_velos, output_repertoire):
         for directory, output_dir in [(repertoire_voitures, output_repertoire + "\\voitures"),
-                                      (repertoire_motos, output_repertoire + "\\motos")]:
+                                      (repertoire_motos, output_repertoire + "\\motos"),
+                                      (repertoire_velos, output_repertoire + "\\velos")]:  # Ajout du répertoire vélos
             for filename in os.listdir(directory):
                 img_path = os.path.join(directory, filename)
                 img = Image.open(img_path)
@@ -28,18 +33,21 @@ class LinearModel:
 
         cars = []
         bikes = []
-        for directory in [output_repertoire + "\\voitures", output_repertoire + "\\motos"]:
+        velos=[]
+        for directory in [output_repertoire + "\\voitures", output_repertoire + "\\motos", output_repertoire + "\\velos"]:
             for filename in os.listdir(directory):
                 img_path = os.path.join(directory, filename)
                 img = Image.open(img_path)
                 img_array = np.array(img) / 255.0
                 if "voitures" in directory:
                     cars.append(img_array.flatten())
+                elif "velos" in directory:
+                      velos.append(img_array.flatten())
                 else:
                     bikes.append(img_array.flatten())
 
-        X = np.vstack((cars, bikes))
-        y = np.hstack((np.zeros(len(cars)), np.ones(len(bikes))))
+        X = np.vstack((cars, bikes, velos))
+        y = np.hstack((np.zeros(len(cars)), np.ones(len(bikes)), np.full(len(velos), 2)))
 
         return X, y
 
@@ -63,11 +71,9 @@ class LinearModel:
         return np.where(predictions >= 0, 1, 0)
 
 if __name__ == "__main__":
-    model = LinearModel(learning_rate=0.001, n_iterations=1000)
+    model = LinearModel(learning_rate=0.01, n_iterations=100000)
+    X, y = model.preprocess_images(repertoire_voitures, repertoire_motos,repertoire_velos, output_repertoire)
 
-    X, y = model.preprocess_images(repertoire_voitures, repertoire_motos, output_repertoire)
-
-    # Mélangez les données une seule fois
     indices = np.arange(X.shape[0])
     np.random.shuffle(indices)
     X = X[indices]
@@ -77,22 +83,19 @@ if __name__ == "__main__":
 
     predictions = model.predict(X)
 
-    class_names = {0: 'Voiture', 1: 'Motos'}
+    class_names = {0: 'Voiture', 1: 'Motos', 2: 'Velos'}
     predicted_classes = [class_names[pred] for pred in predictions]
-
-    # Visualisation des prédictions
     plt.figure(figsize=(14, 8))
     plt.scatter(range(len(predictions)), predictions, c=y, cmap='coolwarm', edgecolors='k')
-    plt.colorbar(label='Classe (0 = Voiture, 1 = Moto)')
+    plt.colorbar(label='Classe (0 = Voiture, 1 = Moto, 2 = Velos)')
     plt.title('Prédictions du modèle linéaire')
     plt.xlabel('Échantillons')
     plt.ylabel('Probabilité prédite')
     plt.show()
 
-    # Visualisation des images et des prédictions
     n_images = len(predictions)
-    n_rows = 3
-    n_cols = (n_images + n_rows - 1) // n_rows  # Ajuster pour avoir le bon nombre de colonnes
+    n_rows = 5
+    n_cols = (n_images + n_rows - 1) // n_rows
 
     plt.figure(figsize=(2 * n_cols, 2 * n_rows))
     for i in range(n_images):
@@ -100,11 +103,9 @@ if __name__ == "__main__":
         plt.xticks([]), plt.yticks([])  # Hide tick marks
         plt.imshow(X[i].reshape(224, 224, 3))
         plt.xlabel(f'Prédiction: {predicted_classes[i]}')
-        # Move the real label to the title to prevent overlap
         plt.title(f'Réel: {class_names[int(y[i])]}')
-    plt.suptitle('Modèle linéaire avec image', y=1.05)  # Adjust y position of the suptitle
+    plt.suptitle('Modèle linéaire avec image', y=1.05)
 
-    # Ajuster l'espacement horizontal
-    plt.subplots_adjust(wspace=0.4, hspace=0.4)  # Augmenter l'espacement entre les sous-graphiques
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
     plt.show()
